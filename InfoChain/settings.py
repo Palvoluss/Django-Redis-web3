c63f69964c132ed7ea4fe91b70d7e0f987be1e4d
Paolo Micheletti <paulmic20@gmail.com>
2022-01-18 08:11:17 +0100

    removed django secret and ./idea

--- InfoChain/settings.py
+++ InfoChain/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
+
+DJANGOPROD = os.environ.get(DJANGOPROD)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_(3o8ugt8#8*i*gxx8$n)3!f()!7*5sqq*s60zzm+&5nu6v+e'
+SECRET_KEY = DJANGOPROD
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
