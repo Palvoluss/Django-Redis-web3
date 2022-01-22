f6daa53caa45f19c6f364d6da88190038e12bdb5
Paolo Micheletti <paulmic20@gmail.com>
2022-01-22 18:28:44 +0100

    corrected imported env vars

--- InfoChain/settings.py
+++ InfoChain/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 
 load_dotenv()
 
-DJANGOPROD = os.environ.get(DJANGOPROD)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGOPROD
+SECRET_KEY = str(os.getenv('DJANGOPROD'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
