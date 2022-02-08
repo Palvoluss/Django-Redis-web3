5670db356675e412450922d8657cf61a956b223d
Pablo Micheletti <pablomctt@gmail.com>
2022-02-08 05:38:13 +0000

    edit on local version to make it work on remote

--- InfoChain/settings.py
+++ InfoChain/settings.py
@@ -139,3 +139,6 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
